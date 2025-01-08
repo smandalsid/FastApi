101adc3d4f641426ec0c14fb70c2802fcf1d8a31
smandalsid <smandal.sid@gmail.com>
2025-01-08 13:49:51 +0530

    restarting

--- ExpenseMgr/expensemgr/main.py
+++ ExpenseMgr/expensemgr/main.py
@@ -1,14 +1,4 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-
-from .database.db import *
-from .database.models import users
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     yield
-
-users.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
